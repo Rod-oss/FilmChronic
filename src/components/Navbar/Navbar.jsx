9b2013db41bb046c84f8717f37051e87c7e89c73
Rod-oss <rodrigo.alvarez0789@gmail.com>
2023-12-08 23:15:53 -0600

    Navbar changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,12 @@
 import CartWidget from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
-import styles from "./Navbar.module.scss";
+import logo from "/logo192.png?url";
+
 
 function Navbar() {
   return (
-    <header
-      className={`navbar navbar-expand-lg fixed-top border-bottom mb-5 ${styles.navbar}`}
-      data-bs-theme="dark"
-    >
-      <nav
-        className="container-fluid d-flex align-items-center justify-content-around"
-        id="navbar"
-      >
+    <header className="navbar navbar-expand-lg fixed-top border-bottom mb-5 navbar-dark">
+      <nav className="container-fluid d-flex align-items-center justify-content-around" id="navbar">
         <button
           className="navbar-toggler"
           type="button"
@@ -21,41 +16,24 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span
-            className={`navbar-toggler-icon ${styles.navbarTogglerIcon}`}
-          ></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <ul
-          className="collapse navbar-collapse navbar-nav col-lg-4 order-2 order-lg-0 justify-content-center gap-4 gap-lg-1 my-3 my-lg-0"
-          id="navbarToggler"
-        >
-          <Link
-            to="/category/Película instantánea"
-            className={` nav-item nav-link ${styles.navLink}`}
-          >
+        <ul className="collapse navbar-collapse navbar-nav col-lg-4 order-2 order-lg-0 justify-content-center gap-4 gap-lg-1 my-3 my-lg-0" id="navbarToggler">
+          <Link to="/category/Película instantánea" className="nav-item nav-link">
             Película instantánea
           </Link>
-          <Link
-            to="/category/35 mm"
-            className={` nav-item nav-link ${styles.navLink}`}
-          >
+          <Link to="/category/35 mm" className="nav-item nav-link">
             35 mm
           </Link>
-          <Link
-            to="/category/120 mm"
-            className={` nav-item nav-link ${styles.navLink}`}
-          >
+          <Link to="/category/120 mm" className="nav-item nav-link">
             120 mm
           </Link>
         </ul>
 
         <div className="col-lg-4 text-center">
-          <Link
-            to="/"
-            className={`fs-1 navbar-brand d-inline-flex py-0 mx-0 ${styles.navbarBrand}`}
-          >
-            <img src={"/logo80.png"} alt="Logo de Film Chronic" />
+          <Link to="/" className="fs-1 navbar-brand d-inline-flex py-0 mx-0">
+            <img src={logo} alt="Logo de Film Chronic" className={styles.NavbarLogo} />
           </Link>
         </div>
 
@@ -68,3 +46,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
